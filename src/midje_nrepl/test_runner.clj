4ca40902e87db6438a8dda56c5760d6000f0d5a3
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2018-03-31 15:54:10 -0300

    A draft of a run-all-tests function.

--- src/midje_nrepl/test_runner.clj
+++ src/midje_nrepl/test_runner.clj
@@ -2,11 +2,19 @@
   (:require [midje-nrepl.reporter :refer [with-reporter-for]])
   (:import clojure.lang.Symbol))
 
-(defn run-tests-from-ns
+(defn run-tests-in-ns
   [^Symbol namespace]
   (with-reporter-for namespace
     (require namespace :reload)))
 
+(defn- merge-reports [a-report other]
+  {:results (merge (:results a-report) (:results other))
+   :summary (merge-with + (:summary a-report) (:summary other))})
+
 (defn run-all-tests
   []
-  )
+  (let [not=zero (complement zero?)]
+    (->> ['octocat.arithmetic-test 'octocat.colls-test 'octocat.mocks-test 'clojure.core]
+         (map run-tests-in-ns)
+         (filter #(-> % :summary :test not=zero))
+         (reduce merge-reports))))
