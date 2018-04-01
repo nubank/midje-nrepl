0042ce647d1061d21b3dd7085ab4c71db5a84e79
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2018-04-01 11:44:10 -0300

    Remove cider-nrepl from the dependencies vector and add clojure.tools.nrepl.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [cider/cider-nrepl "0.16.0"]]
+                 [org.clojure/tools.nrepl "0.2.13"]]
 
   :profiles {:dev {:dependencies [[midje "1.9.2-alpha3"]
                                   [nubank/matcher-combinators "0.2.4"]]}})
