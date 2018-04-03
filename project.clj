04c422a06c36a3d711e09009490ac8c17b264b89
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2018-04-03 17:53:09 -0300

    Add alias to run tests.

--- project.clj
+++ project.clj
@@ -8,4 +8,6 @@
 
   :profiles {:dev {:dependencies [[midje "1.9.2-alpha3"]
                                   [nubank/matcher-combinators "0.2.3"]]
-                   :plugins [                 [lein-midje "3.2.1"]]}})
+                   :plugins [                 [lein-midje "3.2.1"]]}}
+
+  :aliases {"run-tests" ["with-profile" "+dev" "midje" "midje-nrepl.*"]})
