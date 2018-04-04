cf7332afcd69499700b711942020ce1b29359473
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2018-04-04 20:20:09 -0300

    Add Orchard.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.nrepl "0.2.13"]]
+                 [org.clojure/tools.nrepl "0.2.13"]
+                 [cider/orchard "0.1.0-SNAPSHOT"]]
 
   :profiles {:dev {:dependencies [[midje "1.9.2-alpha3"]
                                   [nubank/matcher-combinators "0.2.3"]]
