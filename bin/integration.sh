5c2c74c749858501929afa0243afd03e52295fca
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2018-04-16 18:18:36 -0300

    Improve scripts to run integration tests.

---
+++ bin/integration.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+
+source ${cur_dir}/setup-integration.sh &
+sleep 1 &&
+    lein integration
