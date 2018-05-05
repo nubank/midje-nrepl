d07254f8ce1ea0906964e7da40065e21e6574de7
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2018-05-05 15:17:50 -0300

    Rename macro.

--- src/midje_nrepl/test_runner.clj
+++ src/midje_nrepl/test_runner.clj
@@ -4,7 +4,7 @@
 
 (def test-results (atom {}))
 
-(defmacro ^:private keeping-test-results [& forms]
+(defmacro ^:private caching-test-results [& forms]
   `(let [report# ~@forms]
      (reset! test-results (report# :results))
      report#))
@@ -21,7 +21,7 @@
 (defn run-test
   [^Symbol namespace ^String forms]
   {:pre [(symbol? namespace) (string? forms)]}
-  (keeping-test-results
+  (caching-test-results
    (test-forms namespace (read-string forms))))
 
 (defn- run-tests-in-ns*
@@ -34,7 +34,7 @@
    Returns the test report."
   [^Symbol namespace]
   {:pre [(symbol? namespace)]}
-  (keeping-test-results
+  (caching-test-results
    (run-tests-in-ns* namespace)))
 
 (defn- merge-reports [reports]
@@ -53,7 +53,7 @@
   "Re-runs tests that have failed in the last execution.
   Returns the test report."
   []
-  (keeping-test-results
+  (caching-test-results
    (->> @test-results
         (map #(->> (second %) failed-test-forms (cons (first %))))
         (remove #(= 1 (count %)))
