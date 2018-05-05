593da772bc5a9ddcb7c440daf4123aaa386668da
Alan Ghelardi <alan.ghelardi@nubank.com.br>
2018-05-05 13:18:31 -0300

    Tweak the timeout for starting the REPL and increase the sleep period for starting integration tests.

--- bin/integration.sh
+++ bin/integration.sh
@@ -4,5 +4,5 @@ set -eou pipefail
 cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
 source ${cur_dir}/setup-integration.sh &
-sleep 1 &&
+sleep 3 &&
     lein integration
--- dev-resources/octocat/project.clj
+++ dev-resources/octocat/project.clj
@@ -5,4 +5,6 @@
   :plugins [[lein-midje "3.2.1"]]
 
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :profiles {:dev {:dependencies [[midje "1.9.2-alpha3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.9.2-alpha3"]]}}
+
+  :repl-options {:timeout 3000})
