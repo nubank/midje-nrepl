1cb63576fe1b9324dbb67e1f84ce6edefae5bba7
Alan Ghelardi <alan.ghelardi@gmail.com>
2018-09-22 19:09:39 -0300

    Rename namespace, improve functions and write some tests.

---
+++ src/midje_nrepl/project.clj
@@ -0,0 +1,34 @@
+(ns midje-nrepl.project
+  (:require [clojure.java.io :as io]
+            [clojure.tools.namespace.find :as namespace.find])
+  (:import (java.io FileReader PushbackReader)))
+
+(def ^:private leiningen-project-file "project.clj")
+
+(defn- project-working-dir []
+  (.getCanonicalFile (io/file ".")))
+
+(defn read-leiningen-project []
+  (let [project-file (io/file (project-working-dir) leiningen-project-file)]
+    (with-open [reader (PushbackReader. (FileReader. project-file))]
+      (read reader))))
+
+(defn read-project-map []
+  (let [[_ project-name version & others] (read-leiningen-project)]
+    (into {:project-name+version [project-name version]}
+          (apply hash-map others))))
+
+(defn existing-dir? [candidate]
+  (.isDirectory (io/file (project-working-dir) candidate)))
+
+(defn get-test-paths []
+  (let [project-map (read-project-map)]
+    (->> (get project-map :test-paths ["test"])
+         (filter existing-dir?)
+         sort)))
+
+(defn get-test-namespaces-in [& test-paths]
+  (->> test-paths
+       (map (partial io/file (project-working-dir)))
+       (mapcat namespace.find/find-namespaces-in-dir)
+       sort))
---
+++ test/midje_nrepl/project_test.clj
@@ -0,0 +1,64 @@
+(ns midje-nrepl.project-test
+  (:require [matcher-combinators.matchers :as m]
+            [matcher-combinators.midje :refer [match]]
+            [midje-nrepl.project :as project]
+            [midje.sweet :refer :all]))
+
+(def lein-project-with-test-paths '(defproject octocat "1.0.0"
+                                     :dependencies [[org.clojure/clojure "1.9.0"]]
+                                     :test-paths ["test" "src/test/clojure"]))
+
+(def lein-project-with-no-test-paths '(defproject octocat "1.0.0"
+                                        :dependencies [[org.clojure/clojure "1.9.0"]]))
+
+(facts "about getting information of the current project"
+
+       (fact "reads the project.clj file of the current project and returns its contents as a list"
+             (project/read-leiningen-project)
+             => (match (m/prefix (list 'defproject 'midje-nrepl string?))))
+
+       (fact "reads the project.clj file of the current project and returns it as a map"
+             (project/read-project-map)
+             => (match {:project-name+version ['octocat "1.0.0"]
+                        :dependencies         [['org.clojure/clojure "1.9.0"]]
+                        :test-paths           ["test" "src/test/clojure"]})
+             (provided
+              (project/read-leiningen-project) => lein-project-with-test-paths))
+
+       (tabular (fact "returns true when the given directory exists in the project's working dire"
+                      (project/existing-dir? ?directory) => ?result)
+                ?directory ?result
+                "test" true
+                "integration" true
+                "project.clj" false
+                "non-existing-dir" false)
+
+       (fact "returns the test paths of the project in question, sorted alphabetically"
+             (project/get-test-paths)
+             => ["src/test/clojure" "test"]
+             (provided
+              (project/read-leiningen-project) => lein-project-with-test-paths
+              (project/existing-dir? "test") => true
+              (project/existing-dir? "src/test/clojure") => true))
+
+       (fact "when the project doesn't declare test paths, assumes `test` by default"
+             (project/get-test-paths)
+             => ["test"]
+             (provided
+              (project/read-leiningen-project) => lein-project-with-no-test-paths
+              (project/existing-dir? "test") => true))
+
+       (fact "if the test path is declared in the project.clj, but doesn't exist in the current project, it isn't returned"
+             (project/get-test-paths)
+             => ["test"]
+             (provided
+              (project/read-leiningen-project) => lein-project-with-test-paths
+              (project/existing-dir? "test") => true
+              (project/existing-dir? "src/test/clojure") => false))
+
+       (fact "returns all namespaces declared within the provided test paths sorted alphabetically"
+             (project/get-test-namespaces-in "test/octocat")
+             => ['octocat.arithmetic-test
+                 'octocat.colls-test
+                 'octocat.mocks-test
+                 'octocat.no-tests]))
