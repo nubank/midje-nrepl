cc78de0ac2b79ee2fae4f68e262f9fb732271391
Alan Ghelardi <alan.ghelardi@gmail.com>
2018-10-28 12:20:13 -0300

    Change to Apache License, Version 2.0.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject midje-nrepl "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  [cider/orchard "0.3.0"]
