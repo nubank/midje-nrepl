6ac49fd6d3477feaca663dc62763c73ff6145fb2
Alan Ghelardi <alan.ghelardi@gmail.com>
2018-11-11 20:26:43 -0200

    Fix release settings.

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "nREPL middleware layer to interact with Midje"
   :url "https://github.com/nubank/midje-nrepl"
   :license {:name "Apache License, Version 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :repositories  [["central"  {:url "https://repo1.maven.org/maven2/" :snapshots false}]
-                  ["clojars"  {:url "https://clojars.org/repo/"}]]
+                  ["clojars"  {:url "https://repo.clojars.org"}]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
 
@@ -13,14 +13,14 @@
                  [cider/orchard "0.3.0"]
                  [rewrite-clj "0.6.0"]]
 
-  :profiles {:dev {:dependencies [[diehard "0.7.2"]
-                                  [midje "1.9.4"]
-                                  [nubank/matcher-combinators "0.4.2"]]
-                   :plugins [[lein-midje "3.2.1"]]}
+  :profiles {:dev      {:dependencies [[diehard "0.7.2"]
+                                       [midje "1.9.4"]
+                                       [nubank/matcher-combinators "0.4.2"]]
+                        :plugins      [[lein-midje "3.2.1"]]}
              :provided {:dependencies [[cider/cider-nrepl "0.17.0"]
                                        [refactor-nrepl "2.4.0"]
                                        [leiningen-core "2.8.1"]]}
-             :release {  :repositories  [["clojars"  {:url "https://clojars.org/repo/"
+             :release  {  :repositories [["clojars"  {:url   "https://repo.clojars.org"
                                                       :creds :gpg}]]}}
 
   :test-paths ["integration" "test"])
