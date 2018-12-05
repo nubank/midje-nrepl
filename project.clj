b1d55b74a8f8e66b362346489ca73d93f9f39cdc
Alan Ghelardi <alan.ghelardi@gmail.com>
2018-12-05 18:29:27 -0200

    Version 1.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nubank/midje-nrepl "1.0.1"
+(defproject nubank/midje-nrepl "1.0.2-SNAPSHOT"
   :description "nREPL middleware to interact with Midje"
   :url "https://github.com/nubank/midje-nrepl"
   :license {:name "Apache License, Version 2.0"
