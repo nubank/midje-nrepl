fc14070aca4454cc4af878647493f7d713afc7d3
Alan Ghelardi <alan.ghelardi@gmail.com>
2019-01-02 20:07:41 -0200

    Basic coverage support.

--- src/midje_nrepl/coverage.clj
+++ src/midje_nrepl/coverage.clj
@@ -1,11 +1,23 @@
 (ns midje-nrepl.coverage
   (:require [cloverage.coverage :as coverage]
+            [cloverage.debug :as debug]
+            [cloverage.dependency :as dependency]
+            [cloverage.instrument :as instrument]
             [cloverage.report :as report]
             [cloverage.report.console :as console]
-            [cloverage.instrument :as instrument]
-            [cloverage.dependency :as dependency]))
+            [midje-nrepl.project-info :as project-info]))
 
-(def coverage-report (atom {}))
+(defn- summarize-coverage [forms threshold]
+  (let [totals          (report/total-stats forms)
+        percent-covered (apply min (vals totals))
+        percent-value   #(format "%.2f%%" %)]
+    (-> totals
+        (update :percent-forms-covered percent-value)
+        (update :percent-lines-covered percent-value)
+        (assoc :threshold  threshold)
+        (assoc :status (if (<= percent-covered threshold)
+                         :low-coverage
+                         :acceptable-coverage)))))
 
 (defn- instrument-namespaces [namespaces]
   (let [ordered-namespaces (dependency/in-dependency-order namespaces)]
@@ -13,10 +25,15 @@
       (binding [coverage/*instrumented-ns* namespace]
         (instrument/instrument #'coverage/track-coverage namespace)))))
 
-(defn test-coverage [run-fn]
-  (binding [coverage/*covered* (atom [])]
-    (instrument-namespaces ['midje-nrepl.formatter])
-    (let [report-map (run-fn)
-          forms (report/gather-stats @coverage/*covered*)]
-      (console/summary forms 50 80)
-      report-map)))
+(defn covering [options runner-fn]
+  (let [{:keys [debug? threshold source-namespaces]
+         :or   {debug?            false
+                threshold         50
+                source-namespaces (project-info/find-namespaces-in (project-info/get-source-paths))}} (options :coverage)]
+    (binding [coverage/*covered* (atom [])
+              debug/*debug*      debug?]
+      (instrument-namespaces source-namespaces)
+      (let [report-map (runner-fn)
+            forms      (report/gather-stats @coverage/*covered*)]
+        (assoc report-map
+               :coverage (summarize-coverage forms threshold))))))
