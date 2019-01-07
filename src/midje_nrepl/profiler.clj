5dfefc1118c91f933aeb924ea9332ea8b6d31220
Alan Ghelardi <alan.ghelardi@gmail.com>
2019-01-07 14:01:53 -0200

    Create a profiling wrapper.

--- src/midje_nrepl/profiler.clj
+++ src/midje_nrepl/profiler.clj
@@ -11,25 +11,6 @@
       :else                   (format "%.2f minutes" (/ milliseconds
                                                         60000.0)))))
 
-(defn- duration-of-tests-in-ns [test-results]
-  (let [{:keys [started-at]}  (first test-results)
-        {:keys [finished-at]} (last test-results)]
-    (misc/duration-between started-at finished-at)))
-
-(defn- keep-distinct-tests-with-known-durations [test-results]
-  (->> test-results
-       (filter #(and (:started-at %)
-                     (:finished-at %)))
-       (group-by :id)
-       vals
-       (map last)))
-
-(defn duration-per-namespace [report-map]
-  (->> report-map
-       :results
-       (map #(update % 1 duration-of-tests-in-ns))
-       (map (partial zipmap [:ns :duration]))))
-
 (defn- fastest-test-comparator
   "Compares two test results and determines the fastest one."
   [x y]
@@ -42,13 +23,9 @@
   (comp (partial * -1) fastest-test-comparator))
 
 (defn- top-tests
-  "Returns the top n fastest or slowest tests (depending upon the supplied comparator) in the report map."
-  [comparator n report-map]
-  (->> report-map
-       :results
-       vals
-       flatten
-       keep-distinct-tests-with-known-durations
+  "Returns the top n fastest or slowest tests (depending upon the supplied comparator)."
+  [comparator n test-results]
+  (->> test-results
        (sort comparator)
        (take n)
        (map (fn [{:keys [started-at finished-at] :as result}]
@@ -56,15 +33,57 @@
                      :duration (misc/duration-between started-at finished-at))))))
 
 (def top-fastest-tests
-  "Returns the top n fastest tests in the report map."
+  "Returns the top n fastest tests in the test results."
   (partial top-tests fastest-test-comparator))
 
 (def top-slowest-tests
-  "Returns the top n slowest tests in the report map."
+  "Returns the top n slowest tests in the test results."
   (partial top-tests slowest-test-comparator))
+
+(defn- duration-of-tests-in-ns [test-results]
+  (let [{:keys [started-at]}  (first test-results)
+        {:keys [finished-at]} (last test-results)]
+    (misc/duration-between started-at finished-at)))
+
+(defn duration-per-ns [test-results]
+  (->> test-results
+       (group-by :ns)
+       (map #(update % 1 duration-of-tests-in-ns))
+       (into {})))
 
 (defn average
   "Returns a map describing the average of the duration of tests."
   [duration number-of-tests]
   {:duration (.dividedBy duration number-of-tests)
    :tests    number-of-tests})
+
+(defn distinct-results-with-known-durations [report-map]
+  (->> report-map
+       :results
+       vals
+       flatten
+       (filter #(and (:started-at %)
+                     (:finished-at %)))
+       (group-by :id)
+       vals
+       (map last)))
+
+(defn- assoc-stats [report-map options]
+  (let [{:keys [fastest-tests slowest-tests]
+         :or   {fastest-tests 1
+                slowest-tests 1}} options
+        test-results              (distinct-results-with-known-durations report-map)]
+    (assoc report-map
+           :profiling {:average         (average (get-in report-map [:summary :finished-in]) (count test-results))
+                       :duration-per-ns (duration-per-ns test-results)
+                       :fastest-tests   (top-fastest-tests fastest-tests test-results)
+                       :slowest-tests   (top-slowest-tests slowest-tests test-results)})))
+
+(defn profiling [runner]
+  (fn [options]
+    (let [start      (misc/now)
+          report-map (runner options)
+          end        (misc/now)]
+      (-> report-map
+          (assoc-in [:summary :finished-in] (misc/duration-between start end))
+          (assoc-stats options)))))
--- test/midje_nrepl/profiler_test.clj
+++ test/midje_nrepl/profiler_test.clj
@@ -1,31 +1,32 @@
 (ns midje-nrepl.profiler-test
   (:require [clojure.java.io :as io]
+            [matcher-combinators.midje :refer [match]]
             [midje-nrepl.misc :as misc]
             [midje-nrepl.profiler :as profiler]
             [midje.sweet :refer :all])
   (:import [java.time Duration LocalDateTime]))
 
-(defn local-date-time [seconds]
-  (LocalDateTime/of 2019 01 01 12 0 seconds))
+(defn local-date-time [millis]
+  (LocalDateTime/of 2019 01 01 12 0 0 millis))
 
-(defn plus-seconds [time seconds]
-  (.plusSeconds time seconds))
+(defn plus-millis [time millis]
+  (.plusNanos time (* millis 1000000)))
 
 (def start-point (local-date-time 0))
 
-(def one-second-later (plus-seconds start-point 1))
+(def one-millisecond-later (plus-millis start-point 1))
 
-(def two-seconds-later (plus-seconds start-point 2))
+(def two-milliseconds-later (plus-millis start-point 2))
 
-(def three-seconds-later (plus-seconds start-point 3))
+(def three-milliseconds-later (plus-millis start-point 3))
 
-(def three-seconds+one-millisecond-later (.plusNanos three-seconds-later 1000000))
+(def three-and-half-milliseconds-later (.plusNanos three-milliseconds-later 500000))
 
-(def nine-and-half-seconds-later (plus-seconds start-point 9.5))
+(def nine-milliseconds-later (plus-millis start-point 9))
 
-(def ten-seconds-later (plus-seconds start-point 10))
+(def ten-milliseconds-later (plus-millis start-point 10))
 
-(def thirteen-seconds-later (plus-seconds start-point 13))
+(def thirteen-milliseconds-later (plus-millis start-point 13))
 
 (def arithmetic-test-file (io/file "/home/john-doe/projects/octocat/test/octocat/arithmetic_test.clj"))
 
@@ -34,51 +35,69 @@
 (def report-map {:results
                  {'octocat.arithmetic-test [{:context     ["First arithmetic test"]
                                              :id          "20208edc-4129-4511-be13-38c9a8e28480"
+                                             :ns 'octocat.arithmetic-test
                                              :file        arithmetic-test-file
                                              :line        5
                                              :started-at  start-point
-                                             :finished-at one-second-later}
+                                             :finished-at one-millisecond-later}
                                             {:context ["I am a future fact"]}
                                             {:context     ["second arithmetic test"]
                                              :id          "021a8d7f-e546-42e4-8c70-da4fcc7be6b4"
+                                             :ns 'octocat.arithmetic-test
                                              :file        arithmetic-test-file
                                              :line        8
-                                             :started-at  one-second-later
-                                             :finished-at two-seconds-later}
+                                             :started-at  one-millisecond-later
+                                             :finished-at two-milliseconds-later}
                                             {:context     ["third arithmetic test"]
                                              :id          "b64ac1c8-ff83-4785-8cec-9c180609cb9f"
+                                             :ns 'octocat.arithmetic-test
                                              :file        arithmetic-test-file
                                              :line        10
-                                             :started-at  two-seconds-later
-                                             :finished-at three-seconds-later}
+                                             :started-at  two-milliseconds-later
+                                             :finished-at three-milliseconds-later}
                                             {:context     ["fourth arithmetic test"]
                                              :id          "a3d7e905-8f3f-40b2-bf80-a80566a90a64"
+                                             :ns 'octocat.arithmetic-test
                                              :file        arithmetic-test-file
                                              :line        13
-                                             :started-at  three-seconds-later
-                                             :finished-at three-seconds+one-millisecond-later}]
+                                             :started-at  three-milliseconds-later
+                                             :finished-at three-and-half-milliseconds-later}]
                   'octocat.heavy-test      [{:context     ["First heavy test"]
                                              :id          "8a8e79c5-84aa-4846-b233-5969ec26a853"
+                                             :ns 'octocat.heavy-test
                                              :file        heavy-test-file
                                              :line        5
-                                             :started-at  three-seconds-later
-                                             :finished-at nine-and-half-seconds-later}
+                                             :started-at  three-milliseconds-later
+                                             :finished-at nine-milliseconds-later}
                                             {:context     ["First heavy test"]
                                              :id          "8a8e79c5-84aa-4846-b233-5969ec26a853"
+                                             :ns 'octocat.heavy-test
                                              :file        heavy-test-file
                                              :line        7
-                                             :started-at  three-seconds-later
-                                             :finished-at ten-seconds-later}
+                                             :started-at  three-milliseconds-later
+                                             :finished-at ten-milliseconds-later}
                                             {:context ["I don't have a duration"]
-                                             :id      "0f9878b3-4d28-484d-9b98-a61ef53f4f89"}
+                                             :id      "0f9878b3-4d28-484d-9b98-a61ef53f4f89"
+                                             :ns 'octocat.heavy-test}
                                             {:context ["I don't have a duration too"]
-                                             :id      "2f965b08-3508-43df-969b-274a0a360009"}
+                                             :id      "2f965b08-3508-43df-969b-274a0a360009"
+                                             :ns 'octocat.heavy-test}
                                             {:context     ["second heavy test"]
                                              :id          "12160c1a-4d4a-4d8d-8870-72243ee9539e"
+                                             :ns 'octocat.heavy-test
                                              :file        heavy-test-file
                                              :line        12
-                                             :started-at  ten-seconds-later
-                                             :finished-at thirteen-seconds-later}]}})
+                                             :started-at  ten-milliseconds-later
+                                             :finished-at thirteen-milliseconds-later}]}
+                 :summary {:check 6 :error 0 :fail 0 :ns 2 :pass 6 :to-do 3}})
+
+(def test-results (profiler/distinct-results-with-known-durations report-map))
+
+(defn fake-runner [_]
+  (Thread/sleep (.toMillis (misc/duration-between start-point thirteen-milliseconds-later)))
+  report-map)
+
+(def duration? #(instance? Duration %))
 
 (facts "about the profiler"
 
@@ -90,41 +109,53 @@
                 (Duration/ofMinutes 4)     "4.00 minutes")
 
        (fact "computes the duration of tests for each namespace"
+             (profiler/duration-per-ns test-results)
+             => {'octocat.arithmetic-test  (misc/duration-between start-point three-and-half-milliseconds-later)
+                 'octocat.heavy-test (misc/duration-between three-milliseconds-later thirteen-milliseconds-later)})
 
-             (profiler/duration-per-namespace report-map)
-             => [{:ns       'octocat.arithmetic-test
-                  :duration (misc/duration-between start-point three-seconds+one-millisecond-later)}
-                 {:ns       'octocat.heavy-test
-                  :duration (misc/duration-between three-seconds-later thirteen-seconds-later)}])
-
-       (fact "returns information about the fastest test in the report map"
-             (profiler/top-fastest-tests 1 report-map)
+       (fact "returns information about the fastest test in the test results"
+             (profiler/top-fastest-tests 1 test-results)
              => [{:context  ["fourth arithmetic test"]
                   :file     arithmetic-test-file
                   :line     13
-                  :duration (misc/duration-between three-seconds-later three-seconds+one-millisecond-later)}])
+                  :duration (misc/duration-between three-milliseconds-later three-and-half-milliseconds-later)}])
 
-       (fact "returns information about the slowest test in the report map"
-             (profiler/top-slowest-tests 1 report-map)
+       (fact "returns information about the slowest test in the test results"
+             (profiler/top-slowest-tests 1 test-results)
              => [{:context  ["First heavy test"]
                   :file     heavy-test-file
                   :line     7
-                  :duration (misc/duration-between three-seconds-later ten-seconds-later)}])
+                  :duration (misc/duration-between three-milliseconds-later ten-milliseconds-later)}])
 
-       (fact "returns information about the top n slowest tests in the report map"
-             (profiler/top-slowest-tests 2 report-map)
+       (fact "returns information about the top n slowest tests in the test results"
+             (profiler/top-slowest-tests 2 test-results)
              => [{:context  ["First heavy test"]
                   :file     heavy-test-file
                   :line     7
-                  :duration (misc/duration-between three-seconds-later ten-seconds-later)}
+                  :duration (misc/duration-between three-milliseconds-later ten-milliseconds-later)}
                  {:context  ["second heavy test"]
                   :file     heavy-test-file
                   :line     12
-                  :duration (misc/duration-between ten-seconds-later
-
-                                                   thirteen-seconds-later)}])
+                  :duration (misc/duration-between ten-milliseconds-later
+                                                   thirteen-milliseconds-later)}])
 
        (fact "calculates the average of the duration of tests according to the supplied information"
-             (profiler/average (misc/duration-between start-point ten-seconds-later) 10)
-             => {:duration (misc/duration-between start-point one-second-later)
-                 :tests 10}))
+             (profiler/average (misc/duration-between start-point ten-milliseconds-later) 10)
+             => {:duration (misc/duration-between start-point one-millisecond-later)
+                 :tests 10})
+
+       (fact ""
+             ((profiler/profiling fake-runner) {})
+             => (match {:profiling
+                        {:average {:duration duration?
+                                   :tests 6}
+                         :duration-per-ns {'octocat.arithmetic-test duration?
+                                           'octocat.heavy-test duration?}
+                         :fastest-tests [{:context  ["fourth arithmetic test"]
+                                          :line     13
+                                          :duration duration?}]
+                         :slowest-tests [{:context  ["First heavy test"]
+                                          :line     7
+                                          :duration duration?}]}
+                        :summary {:finished-in duration?}})
+             ))
